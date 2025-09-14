c2d02dc305e5dc60531e20967663daa49880c828
CubeDr <spaceship00@naver.com>
2025-09-14 16:37:06 +0900

    Reformat gameStore.ts

--- src/store/gameStore.ts
+++ src/store/gameStore.ts
@@ -3,32 +3,32 @@ import type { GameState, PlayersMap, ShuttlecocksMap } from '../types/game'
 
 
 interface StoreState {
-myId: string
-nickname: string
-joined: boolean
-players: PlayersMap
-shuttlecocks: ShuttlecocksMap
+  myId: string
+  nickname: string
+  joined: boolean
+  players: PlayersMap
+  shuttlecocks: ShuttlecocksMap
 
 
-setMyId: (id: string) => void
-setNickname: (name: string) => void
-setJoined: (v: boolean) => void
-applyGameState: (s: GameState) => void
-reset: () => void
+  setMyId: (id: string) => void
+  setNickname: (name: string) => void
+  setJoined: (v: boolean) => void
+  applyGameState: (s: GameState) => void
+  reset: () => void
 }
 
 
 export const useGameStore = create<StoreState>((set) => ({
-myId: '',
-nickname: '',
-joined: false,
-players: {},
-shuttlecocks: {},
+  myId: '',
+  nickname: '',
+  joined: false,
+  players: {},
+  shuttlecocks: {},
 
 
-setMyId: (id) => set({ myId: id }),
-setNickname: (name) => set({ nickname: name }),
-setJoined: (v) => set({ joined: v }),
-applyGameState: (s) => set({ players: s.players ?? {}, shuttlecocks: s.shuttlecocks ?? {} }),
-reset: () => set({ players: {}, shuttlecocks: {}, joined: false, nickname: '', myId: '' }),
+  setMyId: (id) => set({ myId: id }),
+  setNickname: (name) => set({ nickname: name }),
+  setJoined: (v) => set({ joined: v }),
+  applyGameState: (s) => set({ players: s.players ?? {}, shuttlecocks: s.shuttlecocks ?? {} }),
+  reset: () => set({ players: {}, shuttlecocks: {}, joined: false, nickname: '', myId: '' }),
 }))
